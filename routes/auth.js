02d29174e42720d0b4a3a293d0b7f8c55aa9e56d
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-04-28 18:26:07 -0700

    fix backend route

--- routes/auth.js
+++ routes/auth.js
@@ -11,52 +11,49 @@ const { createToken } = require("../helpers/tokens");
 const { BadRequestError } = require("../expressError");
 const userAuthSchema = require("../schemas/userAuth.json");
 const userNewSchema = require("../schemas/userNew.json");
-const userRegisterSchema = require("../schemas/userRegister.json")
+const userRegisterSchema = require("../schemas/userRegister.json");
 
-
-
-/** POST /auth/token: { username, password } => { token } 
- * 
+/** POST /auth/token: { username, password } => { token }
+ *
  * Returns JWT token which will be used to authenticate further requests.
- * 
+ *
  * Authorization Required: None.
-*/
+ */
 
-router.post("/token", async function( req, res, next ){
-    try {
-        const validator = jsonschema.validate(req.body, userAuthSchema);
-        if (!validator.valid){
-            const errs = validator.errors.map( e => e.stack );
-            throw new BadRequestError(errs);
-        }
-
-        const { username, password } = req.body;
-        const user = await User.authenticate(username, password);
-        const token = createToken(user);
-        return res.json({ token });
-
-    } catch (err){
-        return next(err);
+router.post("/token", async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userAuthSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
     }
+
+    const { username, password } = req.body;
+    const user = await User.authenticate(username, password);
+    const token = createToken(user);
+    return res.json({ token });
+  } catch (err) {
+    return next(err);
+  }
 });
 
-router.post("/register", async function (req, res, next){
-    console.log("registering a user in auth.js");
+router.post("/register", async function (req, res, next) {
+  console.log("registering a user in auth.js");
 
-    try {
-        const writeObj = {...req.body};
-        const validator = jsonschema.validate(writeObj, userRegisterSchema);
-        if (!validator.validate){
-            const errs = validator.errors.map(e => e.stack);
-            throw new BadRequestError(errs);
-        }
-
-        const newUser = await User.register(writeObj);
-        const token = createToken(newUser);
-        return res.status(201).json({ token })
-    } catch (err){
-        return next(err);
+  try {
+    const writeObj = { ...req.body };
+    const validator = jsonschema.validate(writeObj, userRegisterSchema);
+    if (validator.errors.length != 0) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
     }
-})
+
+    const newUser = await User.register(writeObj);
+    const token = createToken(newUser);
+    return res.status(201).json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
