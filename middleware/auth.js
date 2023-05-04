f28faa2bd0f38862e96bda88d60a2fa01c665ece
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-05-03 21:03:59 -0700

    add middleware for authentication request

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,13 +5,20 @@
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require("../config");
 const { UnauthorizedError } = require("../expressError");
+const { verifyToken } = require("../helpers/tokens");
 
 function authenticateJWT(req, res, next) {
   try {
     const authHeader = req.headers && req.headers.authorizition;
     if (authHeader) {
-      const token = authHeader.replace(/^[Bb]earer /, "").trim();
-      res.locals.user = jwt.verify(token, SECRET_KEY);
+      //   const token = authHeader.replace(/^[Bb]earer /, "").trim();
+      const token = authHeader.replace("Bearer ", "");
+
+      const payload = verifyToken(token);
+
+      req.user = payload;
+
+      //   res.locals.user = jwt.verify(token, SECRET_KEY);
     }
     return next();
   } catch (err) {
