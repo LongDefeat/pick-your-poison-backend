d13bd21a3335fc266a7ac3b5266792f345ffbb2d
LongDefeat <78603508+LongDefeat@users.noreply.github.com>
2023-05-03 18:52:14 -0700

    add middleware for user tokens

--- helpers/tokens.js
+++ helpers/tokens.js
@@ -4,15 +4,22 @@ const { SECRET_KEY } = require("../config");
 /** return signed JWT from user data */
 
 const createToken = (user) => {
-    console.assert(user.isAdmin !== undefined,
-        "createToken passed user without isAdmin property attached")
+  console.assert(
+    user.isAdmin !== undefined,
+    "createToken passed user without isAdmin property attached"
+  );
 
-    let payload = {
-        username: user.username,
-        isAdmin: user.isAdmin || false,
-    };
-    
-    return jwt.sign(payload, SECRET_KEY);
-}
+  let payload = {
+    username: user.username,
+    isAdmin: user.isAdmin || false,
+  };
 
-module.exports = {createToken};
+  return jwt.sign(payload, SECRET_KEY);
+};
+
+/** return payload from token */
+const verifyToken = (token) => {
+  return jwt.verify(token, SECRET_KEY);
+};
+
+module.exports = { createToken, verifyToken };
